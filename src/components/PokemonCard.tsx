ba5e86c83c634f944c78028e0f8fac9b1129a8ef
khaled BRIBRI <khaled.bribri@laposte.net>
2023-10-24 00:11:18 +0200

    PokemonCard component2

--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -1,15 +1,27 @@
+import "./PokemonCard.css";
 function PokemonCard() {
-    return (
-        <>
-            <figure>
-            <img
-                src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
-                alt="Bulbasaur" />
-            <figcaption>
-                Bulbasaur
-            </figcaption>
-            </figure>
-        </>
-    )
+  const pokemonList = [
+    {
+      name: "bulbasaur",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "mew",
+    },
+  ];
+  const pokemon = {
+    name: pokemonList[0].name,
+    imgSrc: pokemonList[0].imgSrc,
+  };
+
+  return (
+    <>
+      <figure className="card">
+        { pokemon.imgSrc  !="" ? <img src={pokemon.imgSrc} alt={pokemon.name} className="cardImg"/>: <p>???</p>}
+        <figcaption>{pokemon.name}</figcaption>
+      </figure>
+    </>
+  );
 }
 export default PokemonCard;
