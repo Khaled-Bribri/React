fe74c45ccb1b020164055c64e85d24058730c26f
khaled BRIBRI <khaled.bribri@laposte.net>
2023-10-26 00:20:00 +0200

    prop-types

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
-import PokemonCard  from "./components/PokemonCard"
+import PokemonCard from "./components/PokemonCard";
+import { useState } from "react";
 
 function App() {
+
   const pokemonList = [
     {
       name: "bulbasaur",
@@ -8,18 +10,53 @@ function App() {
         "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
     },
     {
+      name: "charmander",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+    },
+    {
+      name: "squirtle",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+    },
+    {
+      name: "pikachu",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+    },
+    {
       name: "mew",
     },
   ];
+  const [pokemonIndex, setpokemonIndex] = useState(0);
+
   const pokemon = {
-    name: pokemonList[0].name,
-    imgSrc: pokemonList[0].imgSrc,
+    name: pokemonList[pokemonIndex].name,
+    imgSrc: pokemonList[pokemonIndex].imgSrc,
   };
+
+
+
+  const incremente = () => {
+    if (pokemonIndex > 0) {
+      setpokemonIndex(pokemonIndex - 1);
+    }
+  };
+
+  const decremente = () => {
+    if (pokemonIndex < pokemonList.length - 1) {
+      setpokemonIndex(pokemonIndex + 1);
+    }
+  };
+
   return (
     <>
-      < PokemonCard pokemon = {pokemon} />
+      <PokemonCard pokemon={pokemon} />
+      <div>
+        <button onClick={incremente}>précédent</button>
+        <button onClick={decremente}>suivant</button>
+      </div>
     </>
-  )
+  );
 }
-
-export default App
+export default App;
