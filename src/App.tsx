4f3a49bbbd019a1922ce9ef64c68eae46530437b
khaled BRIBRI <khaled.bribri@laposte.net>
2023-10-27 23:45:50 +0200

    state2

--- src/App.tsx
+++ src/App.tsx
@@ -1,65 +1,38 @@
-import NavBar from "./components/NavBar";
-import PokemonCard from "./components/PokemonCard";
 import { useState } from "react";
 
+import NavBar from "./components/NavBar";
+import PokemonCard from "./components/PokemonCard";
+
+const pokemonList = [
+  {
+    name: "bulbasaur",
+    imgSrc: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "charmander",
+    imgSrc: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+  },
+  {
+    name: "squirtle",
+    imgSrc: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+  },
+  {
+    name: "pikachu",
+    imgSrc: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+  },
+  {
+    name: "mew",
+  },
+];
+
 function App() {
-
-  const pokemonList = [
-    {
-      name: "bulbasaur",
-      imgSrc:
-        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-    },
-    {
-      name: "charmander",
-      imgSrc:
-        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
-    },
-    {
-      name: "squirtle",
-      imgSrc:
-        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
-    },
-    {
-      name: "pikachu",
-      imgSrc:
-        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
-    },
-    {
-      name: "mew",
-    },
-  ];
-  const [pokemonIndex, setpokemonIndex] = useState(0);
-
-  const pokemon = {
-    name: pokemonList[pokemonIndex].name,
-    imgSrc: pokemonList[pokemonIndex].imgSrc,
-  };
-
-
-
-  const incremente = () => {
-    if (pokemonIndex > 0) {
-      setpokemonIndex(pokemonIndex - 1);
-    }
-  };
-
-  const decremente = () => {
-    if (pokemonIndex < pokemonList.length - 1) {
-      setpokemonIndex(pokemonIndex + 1);
-    }
-  };
-
-  const btn = {
-    suivant: decremente,
-    precedent: incremente,
-  }
+  const [pokemonIndex, setPokemonIndex] = useState(0);
 
   return (
-    <>
-      <PokemonCard pokemon={pokemon} />
-      <NavBar btn={btn} />
-    </>
+    <div>
+      <NavBar pokemonIndex={pokemonIndex} setPokemonIndex={setPokemonIndex} pokemonList={pokemonList} />
+      <PokemonCard pokemon={pokemonList[pokemonIndex]} />
+    </div>
   );
 }
 
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,13 +1,35 @@
-import styles from "./NavBar.css";
+import PropTypes from "prop-types";
 
-function NavBar(props) {
+function NavBar({ pokemonIndex, setPokemonIndex, pokemonList }) {
+  const pickPrevious = () => setPokemonIndex(pokemonIndex - 1);
+
+  const pickNext = () => setPokemonIndex(pokemonIndex + 1);
+
   return (
-    <>
-      <div>
-        <button onClick={props.btn.precedent}>précédent</button>
-        <button onClick={props.btn.suivant}>suivant</button>
-      </div>
-    </>
+    <nav>
+      {pokemonIndex > 0 && (
+        <button type="button" onClick={pickPrevious}>
+          previous
+        </button>
+      )}
+      {pokemonIndex < pokemonList.length - 1 && (
+        <button type="button" onClick={pickNext}>
+          next
+        </button>
+      )}
+    </nav>
   );
 }
+
+NavBar.propTypes = {
+  pokemonIndex: PropTypes.number.isRequired,
+  setPokemonIndex: PropTypes.func.isRequired,
+  pokemonList: PropTypes.arrayOf(
+    PropTypes.shape({
+      name: PropTypes.string.isRequired,
+      imgSrc: PropTypes.string,
+    })
+  ).isRequired,
+};
+
 export default NavBar;
